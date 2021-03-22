29e68cc5b3c7a5462595b52887c43315292313d2
mambru <ecastro82@gmail.com>
2021-03-21 20:07:49 -0500

    get route for all products in place

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        as: 'tagged_products',
+        attributes: [
+          'tag_name'
+        ]
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
   // be sure to include its associated Category and Tag data
 });
 
